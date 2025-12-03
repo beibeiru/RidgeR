d3945ff1b06382f8191942099db0c542b2f1928e
Seongyong Park <psychemistz@gmail.com>
2025-12-03 16:35:44 -0500

    Refactor secreted protein inference functions

--- R/hello.R
+++ R/hello.R
@@ -1,129 +1,76 @@
-#' @title Secreted protein activity inference
-#' @description Infer the activity of over 1000 secreted proteins from tumor gene expression profiles.
-#' @param Y Gene expression matrix with gene symbol (row) x sample (column).
+#' @title Secreted protein activity inference (Legacy Version)
+#' @description Old single-threaded implementation.
+#' @param Y Gene expression matrix.
 #' @param SigMat Secreted protein signature matrix.
-#' @param lambda Penalty factor in the ridge regression.
-#' @param nrand Number of randomizations in the permutation test, with a default value 1000.
-#' @return
-#'
-#' A list with four items, each is a matrix.
-#' beta: regression coefficients
-#' se: standard errors of coefficients
-#' zscore: beta/se
-#' pvalue: statistical significance
-#'
-#' @rdname SecAct.inference.gsl.new
+#' @param lambda Penalty factor.
+#' @param nrand Number of randomizations.
 #' @export
-#'
-SecAct.inference.gsl.new <- function(Y, SigMat="SecAct", lambda=5e+05, nrand=1000, coreNo=4)
-{
-  if(SigMat=="SecAct")
-  {
-    Xfile<- file.path(system.file(package = "RidgeR"), "extdata/SecAct.tsv.gz")
-    X <- read.table(Xfile,sep="\t",check.names=F)
-  }else{
-    X <- read.table(SigMat,sep="\t",check.names=F)
-  }
-
-  olp <- intersect(row.names(Y),row.names(X))
-  X <- as.matrix(X[olp,,drop=F])
-  Y <- as.matrix(Y[olp,,drop=F])
-
-  X <- scale(X)
-  Y <- scale(Y)
-
-  ## Prevent NA outputs
-  X[is.na(X)] <- 0
-  Y[is.na(Y)] <- 0
-
-  n <- length(olp)
-  p <- ncol(X)
-  m <- ncol(Y)
-
-  res <- .C("ridgeRegFast",
-            X=as.double(t(X)),
-            Y=as.double(t(Y)),
-            as.integer(n),
-            as.integer(p),
-            as.integer(m),
-            as.double(lambda),
-            as.double(nrand),
-            beta=double(p*m),
-            se=double(p*m),
-            zscore=double(p*m),
-            pvalue=double(p*m),
-            as.integer(coreNo)
-  )
-
-  beta <- matrix(res$beta,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  se <- matrix(res$se,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  zscore <- matrix(res$zscore,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  pvalue <- matrix(res$pvalue,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-
-  res <- list(beta=beta, se=se, zscore=zscore, pvalue=pvalue)
-
-  res
-}
-
-#' @title Secreted protein activity inference
-#' @description Infer the activity of over 1000 secreted proteins from tumor gene expression profiles.
-#' @param Y Gene expression matrix with gene symbol (row) x sample (column).
-#' @param SigMat Secreted protein signature matrix.
-#' @param lambda Penalty factor in the ridge regression.
-#' @param nrand Number of randomizations in the permutation test, with a default value 1000.
-#' @return
-#'
-#' A list with four items, each is a matrix.
-#' beta: regression coefficients
-#' se: standard errors of coefficients
-#' zscore: beta/se
-#' pvalue: statistical significance
-#'
-#' @rdname SecAct.inference.gsl.old
-#' @export
-#'
 SecAct.inference.gsl.old <- function(Y, SigMat="SecAct", lambda=5e+05, nrand=1000)
 {
-  if(SigMat=="SecAct")
-  {
-    Xfile<- file.path(system.file(package = "RidgeR"), "extdata/SecAct.tsv.gz")
+  if(SigMat=="SecAct") {
+    Xfile<- file.path(system.file(package = "SecAct"), "extdata/SecAct.tsv.gz")
     X <- read.table(Xfile,sep="\t",check.names=F)
-  }else{
+  } else {
     X <- read.table(SigMat,sep="\t",check.names=F)
   }
 
   olp <- intersect(row.names(Y),row.names(X))
   X <- as.matrix(X[olp,,drop=F])
   Y <- as.matrix(Y[olp,,drop=F])
+  X <- scale(X); Y <- scale(Y)
+  X[is.na(X)] <- 0; Y[is.na(Y)] <- 0
 
-  X <- scale(X)
-  Y <- scale(Y)
-
-  n <- length(olp)
-  p <- ncol(X)
-  m <- ncol(Y)
+  n <- length(olp); p <- ncol(X); m <- ncol(Y)
 
   res <- .C("ridgeReg",
             X=as.double(t(X)),
             Y=as.double(t(Y)),
-            as.integer(n),
-            as.integer(p),
-            as.integer(m),
-            as.double(lambda),
-            as.double(nrand),
-            beta=double(p*m),
-            se=double(p*m),
-            zscore=double(p*m),
-            pvalue=double(p*m)
+            as.integer(n), as.integer(p), as.integer(m),
+            as.double(lambda), as.double(nrand),
+            beta=double(p*m), se=double(p*m), zscore=double(p*m), pvalue=double(p*m)
   )
 
-  beta <- matrix(res$beta,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  se <- matrix(res$se,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  zscore <- matrix(res$zscore,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-  pvalue <- matrix(res$pvalue,byrow=T,ncol=m,dimnames=list(colnames(X),colnames(Y)))
-
-  res <- list(beta=beta, se=se, zscore=zscore, pvalue=pvalue)
-
-  res
+  formatter <- function(v) matrix(v, byrow=T, ncol=m, dimnames=list(colnames(X), colnames(Y)))
+  list(beta=formatter(res$beta), se=formatter(res$se), zscore=formatter(res$zscore), pvalue=formatter(res$pvalue))
 }
 
+#' @title Secreted protein activity inference (Optimized Version)
+#' @description Fast multi-threaded implementation using OpenMP.
+#' @param Y Gene expression matrix.
+#' @param SigMat Secreted protein signature matrix.
+#' @param lambda Penalty factor.
+#' @param nrand Number of randomizations.
+#' @param ncores Number of cores (default: all available).
+#' @export
+SecAct.inference.gsl.new <- function(Y, SigMat="SecAct", lambda=5e+05, nrand=1000, ncores=NULL)
+{
+  if(SigMat=="SecAct") {
+    Xfile<- file.path(system.file(package = "SecAct"), "extdata/SecAct.tsv.gz")
+    X <- read.table(Xfile,sep="\t",check.names=F)
+  } else {
+    X <- read.table(SigMat,sep="\t",check.names=F)
+  }
+
+  olp <- intersect(row.names(Y),row.names(X))
+  X <- as.matrix(X[olp,,drop=F])
+  Y <- as.matrix(Y[olp,,drop=F])
+  X <- scale(X); Y <- scale(Y)
+  X[is.na(X)] <- 0; Y[is.na(Y)] <- 0
+
+  n <- length(olp); p <- ncol(X); m <- ncol(Y)
+  
+  if(is.null(ncores)) ncores <- parallel::detectCores(logical=FALSE)
+  if(is.na(ncores)) ncores <- 1
+
+  res <- .C("ridgeRegFast",
+            X=as.double(t(X)),
+            Y=as.double(t(Y)),
+            as.integer(n), as.integer(p), as.integer(m),
+            as.double(lambda), as.double(nrand),
+            beta=double(p*m), se=double(p*m), zscore=double(p*m), pvalue=double(p*m),
+            as.integer(ncores)
+  )
+
+  formatter <- function(v) matrix(v, byrow=T, ncol=m, dimnames=list(colnames(X), colnames(Y)))
+  list(beta=formatter(res$beta), se=formatter(res$se), zscore=formatter(res$zscore), pvalue=formatter(res$pvalue))
+}
